8438ca1fbfbf33dc5933baf54063832639759026
Vindhya Lakshmi <vindhyalakshmiofficial@gmail.com>
2025-11-28 19:20:38 +0530

    Time: 0 ms (100%), Space: 57.3 MB (8.49%) - LeetHub

---
+++ 4058-compute-alternating-sum/4058-compute-alternating-sum.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var alternatingSum = function (nums) {
+    let even = 0
+    let odd = 0
+    for (let i = 0; i < nums.length; i++) {
+        if (i % 2 == 0) {
+            even += nums[i]
+        } else {
+            odd -= nums[i]
+        }
+    }
+    return even + odd
+};
