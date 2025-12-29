8936c1ec9e8b7caa12284f1c1f3dfb4233dbee0d
Vindhya Lakshmi <vindhyalakshmiofficial@gmail.com>
2025-12-29 20:41:31 +0530

    Time: 1 ms (97.15%), Space: 60.2 MB (41.83%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum = function (candidates, target) {
+
+    let result = []
+
+    function backtrack(start, path, remaining){
+
+    if(remaining === 0) {
+        result.push([...path])
+        return
+    }
+
+    if (remaining < 0){
+        return
+    }
+
+        for (let i = start; i < candidates.length; i++) {
+            path.push(candidates[i])
+            backtrack(i, path, remaining - candidates[i])
+            path.pop()
+
+        }
+}
+backtrack(0, [], target)
+return result
+};
