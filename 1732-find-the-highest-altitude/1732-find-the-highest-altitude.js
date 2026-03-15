9b6073441fab4bdb23b6952ffc77db55583b61ba
Vindhya Lakshmi <vindhyalakshmiofficial@gmail.com>
2026-03-15 19:19:29 +0530

    Time: 0 ms (100%), Space: 53.5 MB (77.27%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} gain
+ * @return {number}
+ */
+var largestAltitude = function(gain) {
+    let altitude = 0;
+    let maxAltitude = 0;
+
+    for(let i=0;i<gain.length;i++){
+        altitude += gain[i]
+        maxAltitude = Math.max(maxAltitude, altitude)
+    }
+    return maxAltitude
+};
