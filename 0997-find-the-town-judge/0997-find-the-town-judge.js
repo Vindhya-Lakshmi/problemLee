4b633142cbff7b3b756fcf8b4d7c18e2f78f7bb5
Vindhya Lakshmi <vindhyalakshmiofficial@gmail.com>
2026-04-03 19:28:46 +0530

    Time: 6 ms (63.42%), Space: 63.8 MB (43.58%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} n
+ * @param {number[][]} trust
+ * @return {number}
+ */
+var findJudge = function(n, trust) {
+    let count = new Array(n + 1).fill(0);
+
+    for (let [a, b] of trust) {
+        count[a]--; // a trusts someone
+        count[b]++; // b is trusted
+    }
+
+    for (let i = 1; i <= n; i++) {
+        if (count[i] === n - 1) {
+            return i;
+        }
+    }
+
+    return -1;
+
+};
