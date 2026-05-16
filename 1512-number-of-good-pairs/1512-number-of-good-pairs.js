aaefa3863a24156a75023af9b970dbbd62f0a4b4
Vindhya Lakshmi <vindhyalakshmiofficial@gmail.com>
2026-05-16 18:59:41 +0530

    Time: 0 ms (100%), Space: 53.2 MB (80.31%) - LeetHub

---
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var numIdenticalPairs = function(nums) {
+
+    let pairs = {}
+    let count = 0
+    
+    for(let i=0;i<nums.length;i++){
+            if(nums[i] in pairs){
+                
+            count += pairs[nums[i]]
+        
+    }
+    pairs[nums[i]] = (pairs[nums[i]] || 0) + 1
+    }
+    return  count
+};
