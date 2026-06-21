b1d1ade24c90c546589852f6f1bc0f048e32cde7
Vindhya Lakshmi <vindhyalakshmiofficial@gmail.com>
2026-06-21 21:46:34 +0530

    Time: 132 ms (15.91%), Space: 67.4 MB (88.64%) - LeetHub

---
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} costs
+ * @param {number} coins
+ * @return {number}
+ */
+var maxIceCream = function(costs, coins) {
+    costs.sort((a,b)=> a-b)
+
+    let count =0
+    for(let cost of costs){
+        if(cost > coins) break
+
+        count++;
+        coins -= cost
+    }
+    return count
+};
