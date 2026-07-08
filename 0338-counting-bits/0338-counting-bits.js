95dd0190688982878eef1b6637c138d154f6e1e5
Vindhya Lakshmi <vindhyalakshmiofficial@gmail.com>
2026-07-08 16:48:34 +0530

    Time: 3 ms (57.28%), Space: 60.6 MB (77.55%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.js
@@ -0,0 +1,14 @@
+var countBits = function(n) {
+    var dp = new Array(n + 1).fill(0);
+    var sub = 1;
+
+    for (var i = 1; i <= n; i++) {
+        if (sub * 2 === i) {
+            sub = i;
+        }
+
+        dp[i] = dp[i - sub] + 1;
+    }
+
+    return dp;
+};
