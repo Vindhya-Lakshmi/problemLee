1549cf034d34b9ba8e142fffd92372ea226d3e21
Vindhya Lakshmi <vindhyalakshmiofficial@gmail.com>
2026-08-11 15:14:50 +0530

    Time: 1 ms (45.65%), Space: 56.6 MB (17.39%) - LeetHub

---
+++ 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingInteger = function(nums) {
+    let sum = nums[0]
+
+    for(let i=1;i<nums.length;i++){
+        if(nums[i] === nums[i-1] + 1){
+            sum += nums[i]
+        }else {
+            break
+        }
+    }
+    let x = sum;
+    while(nums.includes(x)){
+        x++
+    }
+    return x
+};
